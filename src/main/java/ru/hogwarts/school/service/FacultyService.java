612f7d507f295a4b3e65c611872cefb89e7b7a87
ruslanzverkov <createmed@mail.ru>
2022-12-04 21:29:46 +0300

    service+

--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -1,4 +1,14 @@
 package ru.hogwarts.school.service;
 
+import ru.hogwarts.school.model.Faculty;
+
 public interface FacultyService {
+    Faculty addFaculty(Faculty faculty);
+
+    Faculty findFaculty(long id);
+
+    Faculty editFaculty(long id, Faculty faculty);
+
+    void deleteFaculty(long id);
+
 }
---
+++ src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
@@ -0,0 +1,38 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Faculty;
+
+import java.util.HashMap;
+
+@Service
+public class FacultyServiceImpl implements FacultyService {
+
+    private final HashMap<Long, Faculty> faculties = new HashMap<>();
+    private long count = 0;
+    @Override
+    public Faculty addFaculty(Faculty faculty) {
+        faculty.setId(count++);
+        faculties.put(faculty.getId(), faculty);
+        return faculty;
+    }
+
+    @Override
+    public Faculty findFaculty(long id) {
+        return faculties.get(id);
+    }
+
+    @Override
+    public Faculty editFaculty(long id, Faculty faculty) {
+        if (!faculties.containsKey(id)) {
+            return null;
+        }
+        faculties.put(id, faculty);
+        return faculty;
+    }
+
+    @Override
+    public void deleteFaculty(long id) {
+        faculties.remove(id);
+    }
+}
