d276e6b6863643c08c6cc439157ea260a5afecac
ruslanzverkov <createmed@mail.ru>
2022-12-04 21:00:47 +0300

    Initial commit

---
+++ src/main/java/ru/hogwarts/school/Homeworkplaylist3Application.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homeworkplaylist3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homeworkplaylist3Application.class, args);
+    }
+
+}
